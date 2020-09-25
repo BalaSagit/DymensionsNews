052956cbd283b8d9fa55d2738f49549e242fb797
Balaji <31239389+BalaSagit@users.noreply.github.com>
2020-09-25 18:44:10 +0530

    Updated News items and css

--- src/components/NewsItems/NewsItems.js
+++ src/components/NewsItems/NewsItems.js
@@ -2,28 +2,39 @@ import React, { useState } from "react";
 import "./NewsItems.css";
 import { useSelector } from "react-redux";
 
-
 const IndividualNewsItem = (props) => {
   return (
-    <div className='individual-news-item'>
-      <div className='ini-img-container'>
-        <img src={props.newsItem.urlToImage}
-          alt={props.newsItem.title + " News Item Image"} className='ini-img' />
+    <div className="individual-news-item">
+      <div className="ini-img-container">
+        <img
+          src={props.newsItem.urlToImage}
+          alt={props.newsItem.title + " News Item Image"}
+          className="ini-img"
+        />
       </div>
 
-      <a href={props.newsItem.url} target="_blank" className='ini-title'>{props.newsItem.title}</a>
+      <a href={props.newsItem.url} target="_blank" className="ini-title" rel="noopener noreferrer">
+        {props.newsItem.title}
+      </a>
 
-      <p className='ini-description'>{props.newsItem.description}</p>
-      <div className='ini-footer'>
-        <footer className='ini-footer-left'>
-          <p className='ini-author'>{props.newsItem.author}</p>
-          <p className='ini-pub-at'>{props.newsItem.publishedAt}</p>
+      <p className="ini-description">{props.newsItem.description}</p>
+      <div className="ini-footer">
+        <footer className="ini-footer-left">
+          <p className="ini-author">{props.newsItem.author}</p>
+          <p className="ini-pub-at">{props.newsItem.publishedAt}</p>
         </footer>
-        <a href={props.newsItem.url} target="_blank" className='ini-click-for-more'>more...</a>
+        <a
+          href={props.newsItem.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ini-click-for-more"
+        >
+          more...
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const NewsItems = () => {
   const [selectedCategory] = useSelector(({ newsCategories }) => [
@@ -33,14 +44,34 @@ const NewsItems = () => {
   const [newsList, setNewsList] = useState([]);
 
   React.useEffect(() => {
-    fetch('https://newsapi.org/v2/everything?q=' + selectedCategory + '&pageSize=15&apiKey=a8909a92a0e6475eafe3380fd7a07f8c')
-      .then(results => results.json())
-      .then(data => {
-        setNewsList(data.articles)
+    fetch(
+      "https://newsapi.org/v2/everything?q=" +
+        selectedCategory +
+        "&pageSize=15&apiKey=a8909a92a0e6475eafe3380fd7a07f8c"
+    )
+      .then((results) => results.json())
+      .then((data) => {
+        if (data.articles) {
+          setNewsList(data.articles);
+        }
       });
   }, [selectedCategory]);
 
-  return <div className="news-items-container">{newsList.map((newsItem) => <IndividualNewsItem key={newsItem.title} newsItem={newsItem} />)}</div>;
+  return (
+    <div className="news-items-container">
+      {newsList.length >= 1 ? (
+        newsList.map((newsItem) => (
+          <IndividualNewsItem key={newsItem.title} newsItem={newsItem} />
+        ))
+      ) : (
+        <img
+          src="https://freefrontend.com/assets/img/html-css-404-page-templates/HTML-Error-Page.png"
+          alt= "Empty Page"
+          className="empty-page-img"
+        />
+      )}
+    </div>
+  );
 };
 
 export default NewsItems;
